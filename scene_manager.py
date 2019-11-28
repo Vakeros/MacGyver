273ea24c63d59d454a26e21caf89557596f90911
Defer <jasondefer@hotmail.com>
2019-11-28 11:10:15 +0100

    Player class

---
+++ scene_manager.py
@@ -0,0 +1,15 @@
+from game import *
+
+
+class SceneManager:
+    scenes = [game]
+    currentScene = 0
+    @staticmethod
+    def loadScene(Pid):
+        SceneManager.scenes[SceneManager.currentScene].unload()
+        SceneManager.currentScene = SceneManager.scenes[Pid]
+        SceneManager.scenes[SceneManager.currentScene].init()
+
+    @staticmethod
+    def update(screen):
+        SceneManager.scenes[SceneManager.currentScene].update(screen)
